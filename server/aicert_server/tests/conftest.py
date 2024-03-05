6028038f7cd053b95aef25a61e289fb2f5226e44
Yass <bargach.y@gmail.com>
2024-03-05 16:35:31 +0000

    conftest

--- server/aicert_server/tests/conftest.py
+++ server/aicert_server/tests/conftest.py
@@ -1,14 +1,24 @@
 import pytest 
 from fastapi import FastAPI
+from typing import Generator
 from fastapi.testclient import TestClient
 
 
 def start_application():
     app = FastAPI()
-
     return app
 
 
+@pytest.fixture(scope="module")
+def app() -> Generator[FastAPI, Any, None]:
+    _app = start_application()
+    yield _app
+
+@pytest.fixture(scope="module")
+def client(app: FastAPI) -> Generator[TestClient, Any, None]:
+
+    with TestClient(app) as client:
+        yield client
 
 
 class ModelName:
@@ -17,7 +27,7 @@ class ModelName:
     def __eq__(self, name) -> bool:
         return self.name == name
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def model_name():
     return ModelName("llama2")
 
@@ -27,7 +37,7 @@ class DatasetName:
     def __eq__(self, name) -> bool:
         return self.name == name
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def dataset_name():
     return DatasetName("dataset/alpaca2")
 
